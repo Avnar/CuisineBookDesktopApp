839a17f7adc6a5161b6ddbc1893fc557bceb18ba
Avnar <dragon18189@onet.pl>
2021-05-11 19:44:41 +0200

    wersja z drugim oknem

---
+++ ProjektZespolowy/LoginRegisterWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProjektZespolowy
+{
+    /// <summary>
+    /// Logika interakcji dla klasy LoginRegisterWindow.xaml
+    /// </summary>
+    public partial class LoginRegisterWindow : Window
+    {
+        public LoginRegisterWindow()
+        {
+            InitializeComponent();
+            sethidden();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            Main_Window mainwindow = new Main_Window();
+            mainwindow.Show();
+            this.Close();
+        }
+
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+            register_b.Visibility = Visibility.Hidden;
+            login_b.Visibility = Visibility.Hidden;
+            register_tb.Visibility = Visibility.Visible;
+            registerUnderline.Visibility = Visibility.Visible;
+            save_b.Visibility = Visibility.Visible;
+
+        }
+
+        private void LoginRegisterWindow_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.LeftButton== MouseButtonState.Pressed)
+             DragMove(); 
+        }
+        private void sethidden() 
+        {
+            save_b.Visibility = Visibility.Hidden;
+            register_tb.Visibility = Visibility.Hidden;
+            registerUnderline.Visibility = Visibility.Hidden;
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            login_b.Visibility = Visibility.Visible;
+            register_b.Visibility = Visibility.Visible;
+            sethidden();
+
+        }
+    }
+}
---
+++ ProjektZespolowy/Main Window.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProjektZespolowy
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Main_Window.xaml
+    /// </summary>
+    public partial class Main_Window : Window
+    {
+        public Main_Window()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void addRecipe_b_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void find_b_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void calculator_b_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void exit_b_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
